59ac760880a72781d582a3181f18fc1218ff161b
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 20:49:04 +0200

    Add extension method for checking whether one double value is greater than another

--- Core/Extensions/DoubleCompareExtensions.cs
+++ Core/Extensions/DoubleCompareExtensions.cs
@@ -44,5 +44,40 @@ public static class DoubleCompareExtensions
 
 	#endregion
 
+	#region IsGreaterThan
+
+	/// <summary>
+	/// Returns a value indicating whether the first value is greater than the second one (within the specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to compare</param>
+	/// <param name="otherValue">The value to compare the first value to</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <returns>A value indicating whether the first value is greater than the second one (within the specified tolerance)</returns>
+	/// <exception cref="ArgumentException">Thrown in case the supplied tolerance is not valid</exception>
+	public static bool IsGreaterThan( this double value, double otherValue, double tolerance = Tolerance.Standard )
+	{
+		Tolerance.Validate( tolerance );
+
+		return value - otherValue >= tolerance;
+	}
+
+	/// <summary>
+	/// Returns a value indicating whether the first value is greater than the second one (within the specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to compare</param>
+	/// <param name="otherValue">The value to compare the first value to</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <returns>A value indicating whether the first value is greater than the second one (within the specified tolerance)</returns>
+	/// <exception cref="ArgumentException">Thrown in case the supplied tolerance is not valid</exception>
+	/// <remarks>Please note that this method will return <see langword="false"/> if either (or both) of the values is <see langword="null"/></remarks>
+	public static bool IsGreaterThan( this double? value, double? otherValue, double tolerance = Tolerance.Standard )
+	{
+		Tolerance.Validate( tolerance );
+
+		return value - otherValue >= tolerance;
+	}
+
+	#endregion
+
 	#endregion
 }
