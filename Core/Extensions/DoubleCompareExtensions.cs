cd28bacc3189fa95df4f2a03bbab7bf763c0a6fc
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 21:46:11 +0200

    Add extension method for checking whether double value is non-positive

--- Core/Extensions/DoubleCompareExtensions.cs
+++ Core/Extensions/DoubleCompareExtensions.cs
@@ -250,5 +250,38 @@ public static class DoubleCompareExtensions
 
 	#endregion
 
+	#region IsNonPositive
+
+	/// <summary>
+	/// Returns a value indicating whether the value is non-positive (within the specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to check</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <returns>A value indicating whether the value is non-positive (within the specified tolerance)</returns>
+	/// <exception cref="ArgumentException">Thrown in case the supplied tolerance is not valid</exception>
+	public static bool IsNonPositive( this double value, double tolerance = Tolerance.Standard )
+	{
+		Tolerance.Validate( tolerance );
+
+		return value.IsLessThanOrEqualTo( 0.0, tolerance );
+	}
+
+	/// <summary>
+	/// Returns a value indicating whether the value is non-positive (within the specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to check</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <returns>A value indicating whether the value is non-positive (within the specified tolerance)</returns>
+	/// <exception cref="ArgumentException">Thrown in case the supplied tolerance is not valid</exception>
+	/// <remarks>Please note that this method will return <see langword="false"/> if the value is <see langword="null"/></remarks>
+	public static bool IsNonPositive( this double? value, double tolerance = Tolerance.Standard )
+	{
+		Tolerance.Validate( tolerance );
+
+		return value.IsLessThanOrEqualTo( 0.0, tolerance );
+	}
+
+	#endregion
+
 	#endregion
 }
