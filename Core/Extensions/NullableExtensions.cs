261f396a532a4113c6b9c0b96e2155867af87801
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 18:17:05 +0200

    Add extension method for comparing equality of two nullable classes

--- Core/Extensions/NullableExtensions.cs
+++ Core/Extensions/NullableExtensions.cs
@@ -51,6 +51,24 @@ public static class NullableExtensions
 		where T : struct
 		=> IsEqualTo( one, (T?) other, equalityComparer );
 
+	/// <summary>
+	/// Returns a value indicating whether the given two values are equal
+	/// </summary>
+	/// <param name="one">One of the values to compare</param>
+	/// <param name="other">The other value to compare</param>
+	/// <param name="equalityComparer">The function to use for comparing two non-null values</param>
+	/// <typeparam name="T">The type of the values to compare</typeparam>
+	/// <returns>A value indicating whether the given two values are equal</returns>
+	public static bool IsEqualTo<T>( this T? one, T? other, Func<T, T, bool> equalityComparer )
+		where T : class
+		=> (one, other) switch
+		{
+			(null, null) => true,
+			(null, _)
+				or (_, null) => false,
+			_ => equalityComparer.Invoke( one, other )
+		};
+
 	#endregion
 
 	#endregion
