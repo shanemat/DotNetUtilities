acb7f8cbc575b7c9f3e3644165aed3b04a7b2c41
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-20 21:02:08 +0200

    Add extension method for checking whether one double value is less than another

--- Core/Extensions/DoubleCompareExtensions.cs
+++ Core/Extensions/DoubleCompareExtensions.cs
@@ -114,5 +114,40 @@ public static class DoubleCompareExtensions
 
 	#endregion
 
+	#region IsLessThan
+
+	/// <summary>
+	/// Returns a value indicating whether the first value is less than the second one (within the specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to compare</param>
+	/// <param name="otherValue">The value to compare the first value to</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <returns>A value indicating whether the first value is less than the second one (within the specified tolerance)</returns>
+	/// <exception cref="ArgumentException">Thrown in case the supplied tolerance is not valid</exception>
+	public static bool IsLessThan( this double value, double otherValue, double tolerance = Tolerance.Standard )
+	{
+		Tolerance.Validate( tolerance );
+
+		return otherValue - value >= tolerance;
+	}
+
+	/// <summary>
+	/// Returns a value indicating whether the first value is less than the second one (within the specified tolerance)
+	/// </summary>
+	/// <param name="value">The value to compare</param>
+	/// <param name="otherValue">The value to compare the first value to</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <returns>A value indicating whether the first value is less than the second one (within the specified tolerance)</returns>
+	/// <exception cref="ArgumentException">Thrown in case the supplied tolerance is not valid</exception>
+	/// <remarks>Please note that this method will return <see langword="false"/> if either (or both) of the values is <see langword="null"/></remarks>
+	public static bool IsLessThan( this double? value, double? otherValue, double tolerance = Tolerance.Standard )
+	{
+		Tolerance.Validate( tolerance );
+
+		return otherValue - value >= tolerance;
+	}
+
+	#endregion
+
 	#endregion
 }
