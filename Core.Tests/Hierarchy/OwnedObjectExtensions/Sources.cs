281fe2b426dda244e291ba6b6063c9fcfd5a12bb
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-26 14:15:05 +0200

    Add source objects for owned object tests

---
+++ Core.Tests/Hierarchy/OwnedObjectExtensions/Sources.cs
@@ -0,0 +1,24 @@
+﻿using Shanemat.DotNetUtils.Core.Hierarchy;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Hierarchy.OwnedObjectExtensions;
+
+internal abstract class BaseClass : IOwnedObject
+{
+	#region IOwnedObject
+
+	public object? Owner { get; set; }
+
+	#endregion
+}
+
+internal sealed class ClassA : BaseClass;
+
+internal sealed class ClassB : BaseClass;
+
+internal sealed class ClassC : BaseClass;
+
+internal record struct StructA( object? Owner ) : IOwnedObject;
+
+internal record struct StructB( object? Owner ) : IOwnedObject;
+
+internal record struct StructC( object? Owner ) : IOwnedObject;
