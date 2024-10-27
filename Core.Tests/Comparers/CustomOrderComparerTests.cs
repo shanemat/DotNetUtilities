60b51895b3b9229ccf744762a30206a1484e32e4
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-27 15:40:28 +0100

    Add value comparer which uses a predefined sort order

---
+++ Core.Tests/Comparers/CustomOrderComparerTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Comparers;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Comparers;
+
+/// <summary>
+/// Contains tests for <see cref="CustomOrderComparer{T}"/> class
+/// </summary>
+internal sealed class CustomOrderComparerTests
+{
+	#region Methods
+
+	[Test]
+	public void ShouldThrowWhenDuplicateValuesAreSpecified()
+	{
+		Assert.Throws<ArgumentException>( () => _ = CustomOrderComparer<int>.Create( [1, 2, 1] ) );
+	}
+
+	[Test]
+	public void ShouldNotAffectOrderWhenNoValuesAreSpecified()
+	{
+		var comparer = CustomOrderComparer<int>.Create();
+		int[] array = [9, 5, 8, 1, 3, 2, 6, 4, 0, 5, 1, 8, 3, 9];
+
+		Assert.That( array.Order( comparer ), Is.EqualTo( array ) );
+	}
+
+	[Test]
+	public void ShouldNotAffectOrderWhenOtherValuesAreSpecified()
+	{
+		var comparer = CustomOrderComparer<int>.Create( [10, 12, 15, 17, 20] );
+		int[] array = [9, 5, 8, 1, 3, 2, 6, 4, 0, 5, 1, 8, 3, 9];
+
+		Assert.That( array.Order( comparer ), Is.EqualTo( array ) );
+	}
+
+	[Test]
+	public void ShouldPutOrderedSpecifiedValuesFirst()
+	{
+		var comparer = CustomOrderComparer<int>.Create( [5, 6, 8, 9] );
+		int[] array = [9, 5, 8, 1, 3, 2, 6, 4, 0, 5, 1, 8, 3, 9];
+
+		Assert.That( array.Order( comparer ).Take( 7 ), Is.EqualTo( (int[]) [5, 5, 6, 8, 8, 9, 9] ) );
+	}
+
+	[Test]
+	public void ShouldPutUnSpecifiedValuesSecond()
+	{
+		var comparer = CustomOrderComparer<int>.Create( [5, 6, 8, 9] );
+		int[] array = [9, 5, 8, 1, 3, 2, 6, 4, 0, 5, 1, 8, 3, 9];
+
+		Assert.That( array.Order( comparer ).Skip( 7 ), Is.EqualTo( (int[]) [1, 3, 2, 4, 0, 1, 3] ) );
+	}
+
+	#endregion
+}
---
+++ Core/Comparers/CustomOrderComparer.cs
@@ -0,0 +1,75 @@
+﻿using Shanemat.DotNetUtils.Core.Extensions;
+
+namespace Shanemat.DotNetUtils.Core.Comparers;
+
+/// <summary>
+/// Represents a value comparer which uses a predefined sort order
+/// </summary>
+/// <typeparam name="T">The type of values which are to be sorted</typeparam>
+/// <remarks>Any values not included will be kept at the end of the collection in the same order as they were</remarks>
+public sealed class CustomOrderComparer<T> : IComparer<T>
+	where T : notnull
+{
+	#region Fields
+
+	/// <summary>
+	/// The indices of individual values
+	/// </summary>
+	private readonly IReadOnlyDictionary<T, int> _valueIndices;
+
+	#endregion
+
+	#region Constructors
+
+	/// <summary>
+	/// Creates a new instance of <see cref="CustomOrderComparer{T}"/> class
+	/// </summary>
+	/// <param name="valueIndices">The indices of individual values</param>
+	private CustomOrderComparer( IReadOnlyDictionary<T, int> valueIndices )
+	{
+		_valueIndices = valueIndices;
+	}
+
+	#endregion
+
+	#region Methods
+
+	/// <summary>
+	/// Creates a new instance of <see cref="CustomOrderComparer{T}"/> class
+	/// </summary>
+	/// <param name="values">A collection of values in the desired sort order</param>
+	/// <param name="equalityComparer">The comparer to use for value comparison (if <see langword="null"/> that <see cref="EqualityComparer{T}.Default"/> will be used)</param>
+	public static IComparer<T> Create( IEnumerable<T>? values = null, IEqualityComparer<T>? equalityComparer = null )
+	{
+		var valueArray = values?.ToArray() ?? [];
+
+		if( valueArray.Length != valueArray.Distinct().Count() )
+			throw new ArgumentException( "Duplicate values are not allowed!", nameof( values ) );
+
+		equalityComparer ??= EqualityComparer<T>.Default;
+
+		var valueIndices = valueArray
+			.WithIndices()
+			.ToDictionary( i => i.Element, i => i.Index, equalityComparer );
+
+		return new CustomOrderComparer<T>( valueIndices );
+	}
+
+	#endregion
+
+	#region IComparer<T>
+
+	int IComparer<T>.Compare( T? x, T? y )
+	{
+		var xIndex = GetSortIndex( x );
+		var yIndex = GetSortIndex( y );
+
+		return xIndex.CompareTo( yIndex );
+
+		int GetSortIndex( T? element ) => element is not null
+			? _valueIndices.GetValueOrDefault( element, int.MaxValue )
+			: int.MaxValue;
+	}
+
+	#endregion
+}
