517e8d328275aff44db22eb168e79b51ab75f8d4
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-28 17:47:33 +0100

    Add source values for interval tests

---
+++ Core.Tests/Math/Interval/Sources.cs
@@ -0,0 +1,13 @@
+﻿namespace Shanemat.DotNetUtils.Core.Tests.Math.Interval;
+
+/// <summary>
+/// Contains the source values to use in interval tests
+/// </summary>
+internal static class Sources
+{
+	#region Properties
+
+	internal static IReadOnlyCollection<double> Tolerances { get; } = [1e-3, 1e-12];
+
+	#endregion
+}
