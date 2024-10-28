9309c2a1ca4e4aaaaa1b4e2ad61a3f48735cf1d9
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-28 15:10:09 +0100

    Add support for intervals

---
+++ Core/Math/IInterval.cs
@@ -0,0 +1,31 @@
+﻿namespace Shanemat.DotNetUtils.Core.Math;
+
+/// <summary>
+/// Represents an interval
+/// </summary>
+public interface IInterval
+{
+	#region Properties
+
+	/// <summary>
+	/// Gets the minimum value
+	/// </summary>
+	double Minimum { get; }
+
+	/// <summary>
+	/// Gets a value indicating whether the minimum value is included
+	/// </summary>
+	bool IsMinimumIncluded { get; }
+
+	/// <summary>
+	/// Gets the maximum value
+	/// </summary>
+	double Maximum { get; }
+
+	/// <summary>
+	/// Gets a value indicating whether the maximum value is included
+	/// </summary>
+	bool IsMaximumIncluded { get; }
+
+	#endregion
+}
---
+++ Core/Math/Interval.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace Shanemat.DotNetUtils.Core.Math;
+
+/// <summary>
+/// Represents an interval
+/// </summary>
+public readonly struct Interval : IInterval
+{
+	#region Constructors
+
+	/// <summary>
+	/// Creates a new instance of <see cref="Interval"/> struct
+	/// </summary>
+	/// <param name="minimum">The minimum value</param>
+	/// <param name="isMinimumIncluded">A value indicating whether the minimum value is included</param>
+	/// <param name="maximum">The maximum value</param>
+	/// <param name="isMaximumIncluded">A value indicating whether the maximum value is included</param>
+	private Interval( double minimum, bool isMinimumIncluded, double maximum, bool isMaximumIncluded )
+	{
+		Minimum = minimum;
+		IsMinimumIncluded = isMinimumIncluded;
+		Maximum = maximum;
+		IsMaximumIncluded = isMaximumIncluded;
+	}
+
+	#endregion
+
+	#region Overrides
+
+	public override string ToString() => $"{(IsMinimumIncluded ? '[' : '(')}{Minimum}{CultureInfo.CurrentCulture.TextInfo.ListSeparator} {Maximum}{(IsMaximumIncluded ? ']' : ')')}";
+
+	#endregion
+
+	#region Interval
+
+	public double Minimum { get; } = double.NegativeInfinity;
+
+	public bool IsMinimumIncluded { get; }
+
+	public double Maximum { get; } = double.PositiveInfinity;
+
+	public bool IsMaximumIncluded { get; }
+
+	#endregion
+}
