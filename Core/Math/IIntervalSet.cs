5cd33b6075619bab4de5232a78e774e709476d7d
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-29 22:13:45 +0100

    Add support for interval sets

---
+++ Core/Math/IIntervalSet.cs
@@ -0,0 +1,17 @@
+﻿namespace Shanemat.DotNetUtils.Core.Math;
+
+/// <summary>
+/// Represents a set of intervals
+/// </summary>
+public interface IIntervalSet
+{
+	#region Properties
+
+	/// <summary>
+	/// Gets the list of intervals
+	/// </summary>
+	/// <remarks>The intervals are not overlapping and sorted from the lowest to the highest</remarks>
+	IReadOnlyList<IInterval> Intervals { get; }
+
+	#endregion
+}
---
+++ Core/Math/IntervalSet.cs
@@ -0,0 +1,43 @@
+﻿namespace Shanemat.DotNetUtils.Core.Math;
+
+/// <summary>
+/// Represents a set of intervals
+/// </summary>
+public readonly struct IntervalSet : IIntervalSet
+{
+	#region Constructors
+
+	/// <summary>
+	/// Creates a new instance of <see cref="IntervalSet"/> struct
+	/// </summary>
+	public IntervalSet()
+		: this( [] )
+	{
+		// Intentionally left blank
+	}
+
+	/// <summary>
+	/// Creates a new instance of <see cref="IntervalSet"/> struct
+	/// </summary>
+	/// <param name="intervals">The sorted non-overlapping intervals that make up the set</param>
+	private IntervalSet( IEnumerable<IInterval> intervals )
+	{
+		Intervals = intervals.ToList();
+	}
+
+	#endregion
+
+	#region Overrides
+
+	public override string ToString() => Intervals.Any()
+		? string.Join( " \u222a ", Intervals )
+		: "<Empty>";
+
+	#endregion
+
+	#region IIntervalSet
+
+	public IReadOnlyList<IInterval> Intervals { get; } = new List<IInterval>();
+
+	#endregion
+}
