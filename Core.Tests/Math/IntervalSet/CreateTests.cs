54f36d691b146c14bb8d170dca7d57df09c9f0a5
Matěj Sháněl <Tamy.denet@gmail.com>
2024-10-29 23:21:16 +0100

    Add means to create interval set

---
+++ Core.Tests/Math/IntervalSet/CreateTests.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using Shanemat.DotNetUtils.Core.Math;
+
+namespace Shanemat.DotNetUtils.Core.Tests.Math.IntervalSet;
+
+/// <summary>
+/// Contains tests for <see cref="IntervalSet.Create"/> method
+/// </summary>
+internal sealed class CreateTests
+{
+	#region Sources
+
+	private static IEnumerable<double> Tolerances => Sources.Tolerances;
+
+	#endregion
+
+	#region Tests
+
+	[Test]
+	public void ShouldThrowWhenToleranceIsNotANumber()
+	{
+		Assert.Throws<ArgumentException>( () => Core.Math.IntervalSet.Create( tolerance: double.NaN ) );
+	}
+
+	[Test]
+	public void ShouldThrowWhenToleranceIsNegative()
+	{
+		Assert.Throws<ArgumentException>( () => Core.Math.IntervalSet.Create( tolerance: -1.0 ) );
+	}
+
+	[Test]
+	public void ShouldReturnEmptySetForNoIntervals()
+	{
+		var set = Core.Math.IntervalSet.Create();
+
+		Assert.That( set.Intervals.Any(), Is.False );
+	}
+
+	[Test]
+	public void ShouldReturnSetWithTheSameIntervalsWhenTheyDoNotIntersect()
+	{
+		var firstInterval = Core.Math.Interval.Open( -5, -3 );
+		var secondInterval = Core.Math.Interval.OpenClosed( -1, 1 );
+		var thirdInterval = Core.Math.Interval.Closed( 3, 5 );
+
+		var set = Core.Math.IntervalSet.Create( [firstInterval, secondInterval, thirdInterval] );
+
+		Assert.That( set.Intervals, Is.EqualTo( new[] { firstInterval, secondInterval, thirdInterval } ) );
+	}
+
+	[Test]
+	public void ShouldReturnSortedSet()
+	{
+		var firstInterval = Core.Math.Interval.Open( -7, -5 );
+		var secondInterval = Core.Math.Interval.OpenClosed( -3, -1 );
+		var thirdInterval = Core.Math.Interval.Closed( 1, 3 );
+		var fourthInterval = Core.Math.Interval.ClosedOpen( 5, 7 );
+
+		var set = Core.Math.IntervalSet.Create( [thirdInterval, firstInterval, fourthInterval, secondInterval] );
+
+		Assert.That( set.Intervals, Is.EqualTo( new[] { firstInterval, secondInterval, thirdInterval, fourthInterval } ) );
+	}
+
+	[Test]
+	public void ShouldReturnReducedSet()
+	{
+		var firstInterval = Core.Math.Interval.OpenClosed( -5, -3 );
+		var secondInterval = Core.Math.Interval.ClosedOpen( -3, -1 );
+		var thirdInterval = Core.Math.Interval.Open( 1, 4 );
+		var fourthInterval = Core.Math.Interval.Open( 2, 5 );
+
+		var set = Core.Math.IntervalSet.Create( [thirdInterval, firstInterval, fourthInterval, secondInterval] );
+
+		Assert.That( set.Intervals, Is.EqualTo( new[] { Core.Math.Interval.Open( -5, -1 ), Core.Math.Interval.Open( 1, 5 ) } ) );
+	}
+
+	[Test]
+	[TestCaseSource( nameof( Tolerances ) )]
+	public void ShouldUseProvidedTolerance( double tolerance )
+	{
+		const double minimum = -10.0;
+		const double middle = 0.0;
+		const double maximum = 10.0;
+
+		var narrowerLowerInterval = Core.Math.Interval.OpenClosed( minimum, middle - tolerance * 0.75 );
+		var narrowerUpperInterval = Core.Math.Interval.ClosedOpen( middle + tolerance * 0.75, maximum );
+		var narrowerSet = Core.Math.IntervalSet.Create( [narrowerLowerInterval, narrowerUpperInterval], tolerance );
+
+		var widerLowerInterval = Core.Math.Interval.OpenClosed( minimum, middle - tolerance * 0.25 );
+		var widerUpperInterval = Core.Math.Interval.ClosedOpen( middle + tolerance * 0.25, maximum );
+		var widerSet = Core.Math.IntervalSet.Create( [widerLowerInterval, widerUpperInterval], tolerance );
+
+		Assert.Multiple( () =>
+		{
+			Assert.That( narrowerSet.Intervals, Is.EqualTo( new[] { narrowerLowerInterval, narrowerUpperInterval } ) );
+			Assert.That( widerSet.Intervals, Is.EqualTo( new[] { Core.Math.Interval.Open( minimum, maximum ) } ) );
+		} );
+	}
+
+	#endregion
+}
--- Core/Math/IntervalSet.cs
+++ Core/Math/IntervalSet.cs
@@ -1,4 +1,6 @@
-﻿namespace Shanemat.DotNetUtils.Core.Math;
+﻿using Shanemat.DotNetUtils.Core.Validation;
+
+namespace Shanemat.DotNetUtils.Core.Math;
 
 /// <summary>
 /// Represents a set of intervals
@@ -32,6 +34,58 @@ public readonly struct IntervalSet : IIntervalSet
 	public override string ToString() => Intervals.Any()
 		? string.Join( " \u222a ", Intervals )
 		: "<Empty>";
+
+	#endregion
+
+	#region Methods
+
+	/// <summary>
+	/// Creates a new interval set from the given collection of intervals
+	/// </summary>
+	/// <param name="intervals">The intervals to create a set of</param>
+	/// <param name="tolerance">The tolerance to use</param>
+	/// <returns>The created interval set</returns>
+	/// <exception cref="ArgumentException">Thrown in case the supplied tolerance is not valid</exception>
+	public static IIntervalSet Create( IEnumerable<IInterval?>? intervals = null, double tolerance = Tolerance.Standard )
+	{
+		Tolerance.Validate( tolerance );
+
+		return new IntervalSet( GetSortedReducedIntervals() );
+
+		IEnumerable<IInterval> GetSortedReducedIntervals()
+		{
+			if( intervals is null )
+				yield break;
+
+			var sortedValidIntervals = intervals
+				.OfType<IInterval>()
+				.OrderBy( i => i.Minimum )
+				.ToArray();
+
+			var intervalsCount = sortedValidIntervals.Length;
+
+			if( intervalsCount == 0 )
+				yield break;
+
+			var currentInterval = sortedValidIntervals[0];
+
+			foreach( var interval in sortedValidIntervals.Skip( 1 ) )
+			{
+				if( currentInterval.GetUnionWith( interval, tolerance ) is { } union )
+				{
+					currentInterval = union;
+				}
+				else
+				{
+					yield return currentInterval;
+
+					currentInterval = interval;
+				}
+			}
+
+			yield return currentInterval;
+		}
+	}
 
 	#endregion
 
